0fd8e0fe0ad68c63f2c054769b0d1200b723ca04
matt <matt@email.com>
2019-10-14 20:52:37 -0400

    updated google map

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -3,7 +3,7 @@ import React from 'react';
 var GoogleMap = function () {
   return (
     <div className="map">
-        <iframe title="findus" src="https://www.google.com/maps?q=34.113186+-83.583680&hl=es;z=14&amp;output=embed"></iframe>
+        <iframe title="findus" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.3321646842614!2d-83.58420503864836!3d34.112545454280436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f6090f3362d457%3A0xc7208284fa663aa3!2sProfessional%20Park!5e0!3m2!1sen!2sus!4v1571099894803!5m2!1sen!2sus"></iframe>
     </div>
   );
 }
