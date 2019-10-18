e0689ece319bf9a72ab455103a52f9cf8dbdb8ba
matt <matt@email.com>
2019-10-18 17:55:12 -0400

    unit page update

--- src/routes/Units.js
+++ src/routes/Units.js
@@ -4,6 +4,12 @@ function Units() {
     return (
       <div className="default-body">      
         <h1>Available Units</h1>
+        <div className="unit-div-parent">
+          <div className="unit-div">
+          </div>
+          <div className="unit-div">
+          </div>
+        </div>
       </div>
     );
 }
