d1e3c7c2763cfd6acba98d357ad8c5fc7bbd3301
matt <matt@email.com>
2019-11-10 12:50:57 -0500

    removed home div from default body

--- src/home/Home.js
+++ src/home/Home.js
@@ -6,13 +6,11 @@ import GoogleMap from './GoogleMap';
 
 function Home() {
     return (
-      <div className="default-body">
         <div className="home">
           <Jumbotron />
           <BodyText />
           <GoogleMap />
         </div>
-      </div>
     );
 }
 
