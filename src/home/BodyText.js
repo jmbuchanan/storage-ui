9a7b0b1b5d8844f659568d2eb92e42bdccbaacf8
matt <matt@email.com>
2019-12-26 19:37:06 -0500

    css updates

--- src/home/BodyText.js
+++ src/home/BodyText.js
@@ -3,7 +3,7 @@ import React from 'react';
 var BodyText = function () {
   return (
     <div className="body-text">
-      <h1>Premium self storage for all of your needs</h1>
+      <h1>Premium Self Storage For All Of Your Needs</h1>
       <p>Jefferson Mini Warehouses offers premium climate-controlled storage units
            in Jackson County. With cutting-edge security, professional management, 
            and over 40 units with a variety of sizes, we are here to meet all of your storage needs. 
